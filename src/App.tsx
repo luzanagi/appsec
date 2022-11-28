de6a21d70f2668410d05043483e6861bdc5fd301
Ignacio Zavala <cionagi@hotmail.cl>
2022-11-28 15:43:11 -0300

    Add error

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 
 function App() {
   console.log('eee')
+  const variableAux = 1
+  variableAux = 'eeee'
   return (
     <div className="App">
       <header className="App-header">
